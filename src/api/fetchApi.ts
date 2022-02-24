e92b408830d38dc9a30b1191a40236193db44a80
krungy <furler711@gmail.com>
2022-02-25 01:00:49 +0900

    ✨ feat: api 설정 & froxy 설정 & gitignore 설정
    Co-authored-by: DONGHYUN GO <brad-go@users.noreply.github.com>

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { API_URL } from 'constants/api';
 import { ApiReturnType } from 'types';
 
+const { REACT_APP_API_ENDPOINT } = process.env;
+
 export const fetchApi = async () => {
   return axios.get<ApiReturnType[]>(API_URL).then((res) => res.data);
 };
