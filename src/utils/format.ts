7a322831e56316d003f49d6edf4d6a5ddd4c649e
krungy <furler711@gmail.com>
2022-02-25 18:50:01 +0900

    ✨ feat: 날짜 포멧 변경 utils 함수 기능 추가
    Co-authored-by: DONGHYUN GO <brad-go@users.noreply.github.com>

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,11 +1,19 @@
 export const changeUnixToDate = (number: number): string => {
-  const date: Date = new Date(number * 1000);
-  return date.toString();
+  const date: Date = new Date(number * 1003);
+  const year: number = date.getFullYear();
+  const month: number = date.getMonth() + 1;
+  const day: number = date.getDate();
+  const hour: string = String(date.getHours()).padStart(2, '0');
+  const minute: string = String(date.getMinutes()).padStart(2, '0');
+  const GMT: string = String(date)
+    .slice(-14, -9)
+    .replace(/(.{3})/g, '$1:');
+  return `${year}년 ${month}월 ${day}일 ${hour}:${minute} ${GMT}`;
 };
 
 export const changeToReadableFileSize = (size: number): string => {
-  const BASE = 1024;
-  const idx = Math.floor(Math.log(size) / Math.log(BASE));
+  const BASE: number = 1024;
+  const idx: number = Math.floor(Math.log(size) / Math.log(BASE));
   return (
     (size / Math.pow(BASE, idx)).toFixed(2) + ['B', 'KB', 'MB', 'GB', 'TB'][idx]
   );
