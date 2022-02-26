b5ea357c5e428925b3eda42a918cc70433f894a1
krungy <furler711@gmail.com>
2022-02-26 15:42:35 +0900

    🚑 hotfix: 배포환경 proxy 설정 수정
    Co-authored-by: DONGHYUN GO <brad-go@users.noreply.github.com>

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 import { API_URL } from 'constants/api';
 import { ApiReturnType } from 'types';
 
-const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy/';
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+// const PROXY = '/proxy/';
 const URL = `${PROXY}${API_URL}`;
+console.log(URL);
 
 export const fetchApi = async () => {
   return axios.get<ApiReturnType[]>(URL).then((res) => res.data);
