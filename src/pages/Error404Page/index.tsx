a00009c5e110427ec42a3101ff53ad2fba77b4c5
krungy <furler711@gmail.com>
2022-02-26 14:50:42 +0900

    💄 style: 404 Error Page 스타일 지정
    Co-authored-by: DONGHYUN GO <brad-go@users.noreply.github.com>

--- src/pages/Error404Page/index.tsx
+++ src/pages/Error404Page/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import colors from 'styles/colors';
 import styled from 'styled-components';
 
 const Error404Page = () => {
   return (
     <Error404PageContainer>
-      <ErrorText>
-        404 Error
-        <br />
-        메인으로 가시겠어요?
-      </ErrorText>
+      <ErrorText>404 Error</ErrorText>
       <StyleLink to="/">
-        <button>메인으로 가기</button>
+        <Button>메인으로 가기</Button>
       </StyleLink>
     </Error404PageContainer>
   );
@@ -25,14 +22,26 @@ const Error404PageContainer = styled.div`
 const ErrorText = styled.h1`
   text-align: center;
   margin: 20px 0;
+  font-size: 42px;
+  line-height: 1.2em;
+  font-weight: 700;
+  color: #f53354;
 `;
 
 const StyleLink = styled(Link)`
   text-decoration: none;
-  display: block;
+  display: flex;
+  justify-content: center;
   width: 100%;
 `;
 
-const Button = styled.button``;
+const Button = styled.button`
+  all: unset;
+  padding: 20px;
+  border: 2px solid ${colors.black};
+  border-radius: 12px;
+  font-weight: 600;
+  color: ${colors.black};
+`;
 
 export default Error404Page;
