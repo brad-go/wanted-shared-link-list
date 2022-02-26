362366aa07b7a8f6a8a3f896e9802300f369139b
krungy <furler711@gmail.com>
2022-02-26 16:03:44 +0900

    🚑 hotfix: 배포환경 proxy 제거 및 axios CORS 헤더 추가
    Co-authored-by: DONGHYUN GO <brad-go@users.noreply.github.com>

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -2,11 +2,18 @@ import axios from 'axios';
 import { API_URL } from 'constants/api';
 import { ApiReturnType } from 'types';
 
-const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
-// const PROXY = '/proxy';
-const URL = `${PROXY}${API_URL}`;
-console.log(URL);
+// const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+// // const PROXY = '/proxy';
+// const URL = `${PROXY}${API_URL}`;
+// console.log(URL);
+const { REACT_APP_API_ENDPOINT }: any = process.env;
 
 export const fetchApi = async () => {
-  return axios.get<ApiReturnType[]>(URL).then((res) => res.data);
+  return axios
+    .get<ApiReturnType[]>(REACT_APP_API_ENDPOINT, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    })
+    .then((res) => res.data);
 };
