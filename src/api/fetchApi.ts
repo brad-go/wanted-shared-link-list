97a5632ff80380440c77e5f52dd87db7b0e41cca
고동현 <dhjk35@naver.com>
2022-02-28 17:05:29 +0900

    :package: chore: package.json 수정

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -6,13 +6,6 @@ const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
 
 export const fetchApi = async () => {
   return axios
-    .get<ApiReturnType[]>(`${PROXY}${REACT_APP_API_POINT}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers':
-          'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
-      },
-    })
+    .get<ApiReturnType[]>(`${PROXY}${REACT_APP_API_POINT}`)
     .then((res) => res.data);
 };
